61759caa4915cc93b4b992a9ae6996105ad4476a
rb-adriseyounis <adrise.younis@red-badger.com>
2026-05-13 10:46:48 +0100

    add graphql ide for browser based testing

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-import { graphql, buildSchema } from "graphql";
+import { buildSchema } from "graphql";
+import { createHandler } from "graphql-http/lib/use/express";
+import express from "express";
+import { ruruHTML } from "ruru/server";
 
 const schema = buildSchema(`
   type Query {
@@ -6,16 +9,26 @@ const schema = buildSchema(`
   }
 `);
 
-const rootValue = {
+const root = {
   hello() {
     return "Hello world";
   },
 };
 
-graphql({
-  schema,
-  source: "{ hello }",
-  rootValue,
-}).then((response) => {
-  console.log(JSON.stringify(response, null, 2));
+const app = express();
+
+app.all(
+  "/graphql",
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  }),
+);
+
+app.get("/", (_req, res) => {
+  res.type("html");
+  res.end(ruruHTML({ endpoint: "/graphql" }));
 });
+
+app.listen(4000);
+console.log("Running a graghql api server at localhost:40000/graghql");
